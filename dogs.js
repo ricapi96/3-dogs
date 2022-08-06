f66de6f2ddc9a3ad262b0e98ab5995475e82f844
Erica Roquemore <enroquemore@gmail.com>
2022-08-05 20:37:58 -0400

    trouble with button click

--- dogs.js
+++ dogs.js
@@ -2,11 +2,17 @@ console.log('We are all so sad about Maybe');
 
 // DOM elements
 const tailButton = document.getElementById('tail-wags');
-
+const happy = document.querySelector('#happy');
 
 tailButton.addEventListener('click', happyTails);
+
 
 // Start tails wagging
 function happyTails() {
     console.log('Happy Tails!');
+    const img = document.createElement('img');
+    img.src = this.id + 'milla-maybe.jpg';
+    happy.innerHTML = "";
+    happy.append(img);
+
 }
