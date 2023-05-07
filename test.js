f41274e1e26302ee6092840c53a4ff1c43150c2d
Erica Roquemore <enroquemore@gmail.com>
2023-05-07 15:00:28 -0400

    navbar hamburger menu is a bust

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var navbarMenu = document.getElementById("navbar-menu");
+    // var navbarMenu = document.getElementById("navbar-menu");
     var navbarToggler = document.getElementById("navbar-toggler");
     
     navbarToggler.addEventListener("click", function() {
