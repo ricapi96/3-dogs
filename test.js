4fa538ac6ab41557c03165522a4515e25761f484
Erica Roquemore <enroquemore@gmail.com>
2023-05-07 14:43:07 -0400

    testing hamburger

---
+++ test.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarMenu = document.getElementById("navbar-menu");
+    var navbarToggler = document.getElementById("navbar-toggler");
+    
+    navbarToggler.addEventListener("click", function() {
+      navbarToggler.classList.toggle("collapsed");
+    });
+  });
+  
+
+  
